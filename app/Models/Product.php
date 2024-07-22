2d1cdb7e09105ded28d6d16ac64c14254fdb05e2
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-22 23:04:03 +0700

    feat : Category Model

---
+++ app/Models/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $table="categories",
+              $primaryKey="id",
+              $keyType="int",
+              $incrementing=true,
+              $timestamps=true;
+
+    public function category():BelongsTo{
+        return $this->belongsTo(Category::class, "category_id","id");
+    }
+}
