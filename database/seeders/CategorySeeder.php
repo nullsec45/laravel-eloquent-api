72134bf1410cafc3353d67b362b8e1ff965c5e65
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-24 21:57:25 +0700

    feart: insert data dummy category seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+
 
 class CategorySeeder extends Seeder
 {
@@ -12,6 +14,12 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Category::create([
+            'name' => 'Food'
+        ]);
+
+        Category::create([
+            'name' => 'Gadget'
+        ]);
     }
 }
