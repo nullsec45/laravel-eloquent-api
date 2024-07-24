f4b4a4043b5bbc797801af325d7ec23a83291089
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-24 21:57:57 +0700

    feat: setUp delete all data before test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,15 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\DB;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void{
+        parent::setUp();
+        DB::delete("DELETE FROM products");
+        DB::delete("DELETE FROM categories");
+    }
 }
