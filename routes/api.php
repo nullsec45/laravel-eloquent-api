696aaa9d26aa4f0c4d9be8af85d7bcff1964a067
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-26 22:44:47 +0700

    feat : add routes categories custom, products detail

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\{CategoryController, ProductController};
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/categories', [CategoryController::class,'index']);
+Route::get('/categories/{id}', [CategoryController::class,'show']);
+Route::get('/categories-custom', [CategoryController::class,'categoryCustom']);
+
+Route::get('/products/{id}', [ProductController::class,'show']);
