f0b18f9e87f69eccd77ec03d12edf8f21095532a
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-26 22:40:56 +0700

    feat : new product resource

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+
+    public static $wrap="value";
+    
+    public function toArray(Request $request): array
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "category" => new CategorySimpleResource($this->category),
+            "price" => $this->price,
+            "created_at" => $this->created_at,
+            "updated_at" => $this->updated_at
+        ];
+    }
+}
