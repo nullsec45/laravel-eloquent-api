274fe9f561aa83bcbbb08a8e9e6d582fdd210023
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-26 22:45:19 +0700

    test : test resource collection custom

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -53,4 +53,30 @@ class CategoryTest extends TestCase
                     ]
                 ]);
     }
+
+    public function testResourceCollectionCustom(){
+        $this->seed([CategorySeeder::class]);
+
+        $category=Category::all();
+
+        $this->get('/api/categories-custom')
+             ->assertStatus(200)
+             ->assertJson([
+                    'total' => 2,
+                    'data' => [
+                        [
+                            'id' => $category[0]->id,
+                            'name' => $category[0]->name,
+                            // 'created_at' => $category[0]->created_at->toJSON(),
+                            // 'updated_at' => $category[0]->updated_at->toJSON()
+                        ],
+                        [
+                            'id' => $category[1]->id,
+                            'name' => $category[1]->name,
+                            // 'created_at' => $category[1]->created_at->toJSON(),
+                            // 'updated_at' => $category[1]->updated_at->toJSON()
+                        ]
+                    ]
+             ]);
+    }
 }
