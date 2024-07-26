b95716a91ea35b17ca36af6a56e424075d1f4b30
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-26 22:42:49 +0700

    feat : change model Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,16 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
-    protected $table="categories",
-              $primaryKey="id",
-              $keyType="int",
-              $incrementing=true,
-              $timestamps=true;
+    protected $table = "products";
+    protected $primaryKey = "id";
+    protected $keyType = "int";
+    public $incrementing = true;
+    public $timestamps = true;
 
-    public function category():BelongsTo{
-        return $this->belongsTo(Category::class, "category_id","id");
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, "category_id", "id");
     }
 }
