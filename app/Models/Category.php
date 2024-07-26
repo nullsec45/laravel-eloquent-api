da620e4b3d3b272c63d3fc3e871b27b3eb929680
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-26 22:41:33 +0700

    feat : add relation categories to products

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,14 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
-    public $table="categories",
-           $incrementing=true,
-           $timestamps=true;
+    protected $table = "categories";
+    protected $primaryKey = "id";
+    protected $keyType = "int";
+    public $incrementing = true;
+    public $timestamps = true;
 
-    protected $primaryKey="id",
-              $keyType="int";
-       
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class, "category_id", "id");
+    }
 }
