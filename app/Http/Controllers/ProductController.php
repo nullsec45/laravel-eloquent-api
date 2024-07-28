0ebfce5ced250761191d2107acb322a611469628
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-28 09:19:54 +0700

    feat: add method index * paging in product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,14 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Resources\ProductResource;
+use App\Http\Resources\{ProductResource, ProductCollection};
 use App\Models\{Category, Product};
 
 
 class ProductController extends Controller
 {
+    public function index(){
+        $products=Product::all();
+        return new ProductCollection($products);
+    }
+
     public function show(string $id){
         $products=Product::find($id);
         return new ProductResource($products);
+    }
+
+    public function paging(Request $request){
+        $page=$request->get("page", 2);
+        $products=Product::paginate(perPage:2, page:$page);
+        return new ProductCollection($products);
     }
 }
