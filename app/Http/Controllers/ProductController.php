78312caf757bfee861772fedd118b15530e0215a
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-30 22:42:09 +0700

    feat : create method productDebug

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Resources\{ProductResource, ProductCollection};
+use App\Http\Resources\{ProductResource, ProductCollection, ProductDebugResource};
 use App\Models\{Category, Product};
 
 
@@ -16,12 +16,18 @@ class ProductController extends Controller
 
     public function show(string $id){
         $products=Product::find($id);
-        return new ProductResource($products);
+        $products->load("category");
+        return (new ProductResource($products))->response()->header("X-Powered-By","Fajar");
     }
 
     public function paging(Request $request){
         $page=$request->get("page", 2);
         $products=Product::paginate(perPage:2, page:$page);
         return new ProductCollection($products);
+    }
+
+    public function productDebug(string $id){
+        $product=Product::find($id);
+        return new ProductDebugResource($product);
     }
 }
