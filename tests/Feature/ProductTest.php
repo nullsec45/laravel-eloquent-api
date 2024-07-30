edd51bc17763d90c8ac1d70c0e6b03809c473b77
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-07-30 22:44:00 +0700

    test: test additional meta data & conditional attributes

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -61,4 +61,63 @@ class ProductTest extends TestCase
        self::assertNotNull($response->json("meta"));
        self::assertNotNull($response->json("data"));
     }
+
+    public function testAdditionalMetaData(){
+        $this->seed([CategorySeeder::class, ProductSeeder::class]);
+
+        $product=Product::first();
+        $response=$this->get("/api/products-debug/$product->id")
+             ->assertStatus(200)
+             ->assertJson([
+                "author" => "Fajar",
+                "data" => [
+                    "id" => $product->id,
+                    "name" => $product->name,
+                    "price" => $product->price
+                ]
+             ]);
+
+        self::assertNotNull($response->json("server_time"));
+    }
+
+    public function testConditionalAttributes(): void
+    {
+        $this->seed([CategorySeeder::class, ProductSeeder::class]);
+
+        $product=Product::first();
+
+
+        $this->get("/api/products/$product->id")
+             ->assertStatus(200)
+             ->assertJson([
+                "value" => [
+                    "name" => $product->name,
+                    "category" => [
+                        "id" => $product->category->id,
+                        "name" => $product->category->name,
+                    ],
+                    "price" => $product->price,
+                    "is_expensive" => $product->price > 200,
+                    "created_at" => $product->created_at->toJSON(),
+                    "updated_at" => $product->updated_at->toJSON()
+                ]
+             ]);
+    }
+
+    public function testResourceCollection(){
+        $this->seed([CategorySeeder::class, ProductSeeder::class]);
+        $response=$this->get("/api/products/")
+                       ->assertStatus(200)
+                       ->assertHeader("X-Powered-By","Fajar");
+
+        $names=$response->json("data.*.name");
+
+        for($i=0;$i < 5; $i++){
+            self::assertContains("Product $i of category Food", $names);
+        }
+
+        for($i=0;$i < 5; $i++){
+            self::assertContains("Product $i of category Gadget", $names);
+        }
+    }
 }
